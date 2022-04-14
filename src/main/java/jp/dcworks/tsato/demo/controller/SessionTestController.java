b5b2caa7165540fac95849c781ba9280fb7595bc
Tomomichi Sato <tsato@dcworks.jp>
2022-04-14 15:29:17 +0900

    SpringBoot検証 →セッション関連の検証。 　@SessionAttributes を使ってみたが、実装方法が間違っているのか、 　あまり期待した動きになっていない。

--- src/main/java/jp/dcworks/tsato/demo/controller/SessionTestController.java
+++ src/main/java/jp/dcworks/tsato/demo/controller/SessionTestController.java
@@ -3,12 +3,15 @@ package jp.dcworks.tsato.demo.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.dcworks.tsato.demo.dto.RequestForm;
@@ -20,13 +23,23 @@ import jp.dcworks.tsato.demo.dto.RequestForm;
  */
 @Controller
 @RequestMapping("/session")
+@SessionAttributes(value = {"requestForm"})
 public class SessionTestController {
 	private static final Logger logger = LogManager.getLogger(SessionTestController.class.getName());
 
+	@ModelAttribute(value = "requestForm")
+    public RequestForm setUpRequestForm() {
+		logger.info("setUpRequestForm");
+        return new RequestForm();
+    }
+
 	@GetMapping("form")
 	public String form(
-			@ModelAttribute RequestForm requestForm) {
+			@ModelAttribute("requestForm") RequestForm requestForm
+			, Model model) {
 		logger.info("session/form");
+
+		model.addAttribute(requestForm);
 
 		return "session/form";
 	}
@@ -34,10 +47,15 @@ public class SessionTestController {
 
 	@PostMapping("regist")
 	public String regist(
-			@Validated @ModelAttribute RequestForm requestForm
+			@Validated @ModelAttribute("requestForm") RequestForm requestForm
 			, BindingResult result
-			, RedirectAttributes redirectAttributes) {
+			, RedirectAttributes redirectAttributes
+			, SessionStatus sessionStatus) {
 		logger.info("session/regist");
+
+		if (requestForm == null) {
+			logger.info("requestFormは破棄されている。");
+		}
 
 		// バリデーション
 		if (result.hasErrors()) {
@@ -47,11 +65,18 @@ public class SessionTestController {
 			redirectAttributes.addFlashAttribute("errors", result);
 			redirectAttributes.addFlashAttribute("requestForm", requestForm);
 
+			// セッション破棄
+			sessionStatus.setComplete();
+
 			// 「session/form」にリダイレクト
 			return "redirect:/session/form";
 		}
 
 		redirectAttributes.addFlashAttribute("isSuccess", "1");
+
+		// セッション破棄
+		sessionStatus.setComplete();
+
 		return "redirect:/";
 	}
 }
