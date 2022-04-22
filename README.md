f7787e54e74ca2a8f805cefd3debe13006f3ec83
Tomomichi Sato <tsato@dcworks.jp>
2022-04-22 11:47:55 +0900

    DB接続周り調整。

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SpringBootDemo
 SpringBoot検証用
+
+## 手順
+
+1. docker起動（DB起動）
+```
+>cd spring_boot_demo_docker
+>docker-compose up --build
+```
+
+2. アプリ起動  
+eclipseで起動。
--- src/main/java/jp/dcworks/tsato/demo/AppSecurityConfig.java
+++ src/main/java/jp/dcworks/tsato/demo/AppSecurityConfig.java
@@ -1,6 +1,7 @@
 package jp.dcworks.tsato.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		// デフォルトのログイン画面を無効にする処理。
 		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// デフォルトのユーザを削除してログに表示されないようにする。
+		auth.inMemoryAuthentication();
 	}
 }
