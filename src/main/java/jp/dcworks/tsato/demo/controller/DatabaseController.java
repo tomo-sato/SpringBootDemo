bd6f8c247600d66b2e5d6092592f08139ef00b22
Tomomichi Sato <tsato@dcworks.jp>
2022-04-26 19:43:43 +0900

    SpringBoot検証 →ソート機能の実装。

--- src/main/java/jp/dcworks/tsato/demo/controller/DatabaseController.java
+++ src/main/java/jp/dcworks/tsato/demo/controller/DatabaseController.java
@@ -1,5 +1,11 @@
 package jp.dcworks.tsato.demo.controller;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import javax.persistence.Column;
+
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +29,7 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 @RequestMapping("/db")
 public class DatabaseController {
+	private static final String DEFAULT_PAGE = "0";
 
 	@Autowired
 	private UserService userService;
@@ -40,20 +47,75 @@ public class DatabaseController {
 		return "db/index";
 	}
 
+	/**
+	 * ※クエリパラメータ名はBacklogと、
+	 * {@link org.springframework.data.domain.PageRequest#of(int page, int size, Sort sort)}参照。
+	 *
+	 * ※Requestパラメータが変換できないと「400 Bad Request」になるので、Stringで受け付けて、変換できなかったらデフォルトに置き換える。
+	 *
+	 * ※TODO {@code page}や、{@code size}は、プロパティファイルなどで管理したい。
+	 *
+	 * @param page ページ番号（0始まり、変換できなかったら0で。）
+	 * @param size ページサイズ（limit、変換できなかったら25で。）
+	 * @param sort カラム名（存在しないカラムの場合、500 Internal Server Errorになる。存在しなかったらidで。）
+	 * @param order 文字列指定"true".昇順/"false".降順
+	 * （※Booleanにすると、変換できない値の場合400 Bad Requestになる。変換できなかったら昇順で。）
+	 * @param model
+	 * @return
+	 */
 	@GetMapping(value="test2")
 	public String getAllEmployees(
-			@RequestParam(defaultValue = "0") Integer pageNo,
-			@RequestParam(defaultValue = "10") Integer pageSize,
-			@RequestParam(defaultValue = "id") String sortBy,
+			@RequestParam(defaultValue = DEFAULT_PAGE) String page,
+			@RequestParam(defaultValue = "5") String size,
+			@RequestParam(defaultValue = "id") String sort,
+			@RequestParam(defaultValue = "true") String order,
 			Model model) {
 
-		Page<User> userPage = userService.findAll(pageNo, pageSize, sortBy);
+		// とりあえず、parseInt。要エラー処理。
+		int ipage = Integer.parseInt(page);
+		int isize = Integer.parseInt(size);
+		boolean isOrderAsc = BooleanUtils.toBoolean(order);
+
+		// TODO カラムが存在するかどうかチェック
+		String sortColumn = isColumnExist(User.class, sort) ? sort : "id";
+
+		Page<User> userPage = userService.findAll(ipage, isize, sortColumn, isOrderAsc);
 		model.addAttribute("users", userPage);
 		model.addAttribute("page", userPage);
-		model.addAttribute("sortBy", sortBy);
+		model.addAttribute("sort", sortColumn);
+		model.addAttribute("order", order);
 		model.addAttribute("words", userPage.getContent());
 		model.addAttribute("url", "");
 
 		return "db/index2";
+	}
+
+
+	private static boolean isColumnExist(Class<User> clazz, String columnName) {
+		boolean isColumnExist = false;
+
+		Field[] fieldList = clazz.getDeclaredFields();
+		for (Field field : fieldList) {
+			// フィールドの属性（修飾子）を取得
+			int mod = field.getModifiers();
+
+			// 静的要素は除外
+			if (Modifier.isStatic(mod)) {
+				continue;
+			}
+
+			Column column = field.getAnnotation(Column.class);
+			if (column != null) {
+				String name = column.name();
+				if (name != null) {
+					if (name.equals(columnName)) {
+						isColumnExist = true;
+						break;
+					}
+				}
+			}
+
+		}
+		return isColumnExist;
 	}
 }
--- src/main/java/jp/dcworks/tsato/demo/service/UserService.java
+++ src/main/java/jp/dcworks/tsato/demo/service/UserService.java
@@ -1,9 +1,14 @@
 package jp.dcworks.tsato.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
 
 import jp.dcworks.tsato.demo.entity.User;
@@ -28,8 +33,28 @@ public class UserService {
 		return userList;
 	}
 
-	public Page<User> findAll(Integer pageNo, Integer pageSize, String sortBy) {
-		Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+	/**
+	 * ●●テーブルの情報を取得する.
+	 *
+	 * @param page ページ番号
+	 * @param size ページサイズ
+	 * @param sort ソート対象のカラム
+	 * @param isOrderAsc 昇順、降順（true.asc/false.desc）
+	 * @return ●●テーブルの情報を返す。
+	 */
+	public Page<User> findAll(Integer page, Integer size, String sort, boolean isOrderAsc) {
+
+		Direction direction = isOrderAsc ? Direction.ASC : Direction.DESC;
+		List<Order> orderList = new ArrayList<Order>();
+
+		// ID以外であれば、追加で設定。
+		if (sort != null && !"id".equals(sort)) {
+			orderList.add(new Order(direction, sort));
+		}
+		// IDはデフォルト設定。
+		orderList.add(new Order(direction, "id"));
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(orderList));
 		Page<User> userList = repository.findAll(pageable);
 		return userList;
 	}
